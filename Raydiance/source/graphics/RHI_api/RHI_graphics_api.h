faf2b7d057c8268a3fd3f9cb99604afa3be855ec
pepijn <p.averink@gmail.com>
2026-05-16 17:39:21 +0200

    Compile time macros for graphics API's

--- Raydiance/source/graphics/RHI_api/RHI_graphics_api.h
+++ Raydiance/source/graphics/RHI_api/RHI_graphics_api.h
@@ -1,5 +1,10 @@
 #pragma once
+#include "./raydiance.h"
+
+
+// Core includes
 #include "./core/container/types.h"
+
 
 namespace Raydiance
 {
@@ -11,6 +16,7 @@ namespace Raydiance
 			RHI_GRAPHICS_API_VULKAN		= 0x01,
 			RHI_GRAPHICS_API_DIRECTX12  = 0x02,
 		};
+
 
 		inline std::string RHI_GraphicsAPI_ToString(RHI_GraphicsAPI _api)
 		{
@@ -27,6 +33,7 @@ namespace Raydiance
 			}
 		}
 
+
 		inline bool RHI_GraphicsAPI_IsValid(RHI_GraphicsAPI _api)
 		{
 			switch (_api)
@@ -34,6 +41,28 @@ namespace Raydiance
 				case RHI_GraphicsAPI::RHI_GRAPHICS_API_VULKAN:
 				case RHI_GraphicsAPI::RHI_GRAPHICS_API_DIRECTX12:
 					return true;
+			}
+
+			return false;
+		}
+
+
+		inline bool RHI_GraphicsAPI_IsPresent(RHI_GraphicsAPI _api)
+		{
+			switch (_api)
+			{
+			case RHI_GraphicsAPI::RHI_GRAPHICS_API_VULKAN:
+#if defined(COMPILE_GRAPHICS_API_VK)
+				return true;
+#else
+				return false;
+#endif
+			case RHI_GraphicsAPI::RHI_GRAPHICS_API_DIRECTX12:
+#if defined(COMPILE_GRAPHICS_API_DX12)
+				return true;
+#else
+				return false;
+#endif
 			}
 
 			return false;
--- Raydiance/source/raydiance.h
+++ Raydiance/source/raydiance.h
@@ -9,8 +9,19 @@
 #define BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
 
 
-
+// Enable vulkan
 #define COMPILE_WITH_VULKAN
+#if defined(COMPILE_WITH_VULKAN)
+#define COMPILE_GRAPHICS_API_VK
+#endif
+
+
+// Enable directx12
 #define COMPILE_WITH_DIRECTX12
+#if defined(COMPILE_WITH_DIRECTX12)
+#define COMPILE_GRAPHICS_API_DX12
+#endif
+
+
 #define DEFAULT_GRAPHICS_API RHI_GraphicsAPI::RHI_GRAPHICS_API_VULKAN
 //#define DEFAULT_GRAPHICS_API RHI_GraphicsAPI::RHI_GRAPHICS_API_DIRECTX12
