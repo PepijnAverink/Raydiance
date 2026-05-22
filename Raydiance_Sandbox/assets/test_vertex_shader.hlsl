37728e58702b62854b9610d310a0b7b4986fb0a8
Pepijn <p.averink@gmail.com>
2026-05-22 20:52:20 +0200

    Changed test shader to current version of vertexShader.

--- Raydiance_Sandbox/assets/test_vertex_shader.hlsl
+++ Raydiance_Sandbox/assets/test_vertex_shader.hlsl
@@ -1,18 +1,38 @@
-struct VSInput
+#if defined(VULKAN)
+#define CONSTANTS(name, type) [[vk::push_constant]] type name;
+#elif defined(D3D12)
+#define CONSTANTS(name, type) ConstantBuffer<type> name : register(b999, space999)
+#endif
+
+struct FrameData
 {
-    float3 position : POSITION;
+    float4x4 viewProj;
+    float4x4 model; // HLSL uses 'float4x4' for 4x4 matrices
 };
 
-struct VSOutput
+CONSTANTS(frame, FrameData);
+
+
+// Input structure for vertex shader
+// These correspond to the vertex attributes (layout(location = X) in)
+struct VertexShaderInput
 {
-    float4 position : SV_POSITION;
+    float3 inPosition : POSITION; // Or TEXCOORD0, or a custom semantic like 'ATTRIB0'
+    float2 inTexCoord : TEXCOORD0; // Corresponds to layout(location = 2)
 };
 
-VSOutput main(VSInput input)
+struct PixelShaderInput
 {
-    VSOutput output;
+    float4 position : SV_POSITION; // gl_Position in GLSL
+    float2 texCoord : TEXCOORD0;
+};
 
-    output.position = float4(input.position, 1.0f);
-
+// Main vertex shader function
+PixelShaderInput main(VertexShaderInput input)
+{
+    PixelShaderInput output;
+    output.position = mul(mul(float4(input.inPosition, 1.0), frame.model), frame.viewProj);
+    output.texCoord = input.inTexCoord;
+    
     return output;
 }
